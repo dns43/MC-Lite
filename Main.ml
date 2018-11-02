93027398a2fc4a2a3ef6b1575b828ff20de1ffd0
dns43 <dennis.roellke@rub.de>
2018-11-02 18:57:12 +0100

    call parser

--- Main.ml
+++ Main.ml
@@ -4,7 +4,9 @@ open Utils
 
 let () =	
 		let filename 	= "test.mc" in
-      let file_in	= fun () -> open_in filename in
-      let lexbuf  = fun () -> Lexing.from_channel (file_in ()) in
-      let token_list = fun () -> Processor.build_token_list (lexbuf ()) in
-        print_string( Utils.token_list_to_string (token_list()) )
+        let file_in	= fun () -> open_in filename in
+        let lexbuf  = fun () -> Lexing.from_channel (file_in ()) in
+        let token_list = fun () -> Processor.build_token_list (lexbuf ()) in
+            (*print_string( Utils.token_list_to_string (token_list()) )*)
+        let program = fun () -> Processor.parser filename (token_list ()) in
+            print_string( Utils.print_tree (program()) )
