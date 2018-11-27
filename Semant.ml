6677d8a95bbe10754fab7e6adced0144dc2f9035
dns43 <dennis.roellke@rub.de>
2018-11-27 23:26:41 +0100

    Symbol Table

--- Semant.ml
+++ Semant.ml
@@ -21,38 +21,78 @@ let check_program = function
     (*in*)
 
     (*check_dups (List.sort (fun (_,a) (_,b) -> compare a b) top_stmts);*)
+(*
+      let built_in_decls = StringMap.add "print"
+        (*{ typ = Void; fname = "print"; formals = [(Int, "x")];
+      locals = []; body = [] } (StringMap.singleton "printb"
+      { typ = Void; fname = "printb"; formals = [(Bool, "x")];
+      locals = []; body = [] })*)
+      in
+
 
     let symbols = StringMap.empty and function_decls = StringMap.empty
     in
+    *
+    let function_decls =
+        List.fold_left (fun m fd -> StringMap.add fd.fname fd m)
+        built_in_decls functions
+    in
+
+    let function_decl s = try StringMap.find s function_decls
+        with Not_found -> raise (Failure ("unrecognized function " ^ s))
+    in
+*)
+    let add_symbol m stmt = match stmt with
+        Local (t, n, e) -> StringMap.add n t m
+        | _ -> m
+    in
+
+
+    let add_functions m stmt = match stmt with
+        Function f -> StringMap.add f.fname f.returnType m
+        | _ -> m
+    in
+
+    let function_decls = List.fold_left add_functions StringMap.empty top_stmts in
+
+    (* Variable symbol table: type of each global, formal, local *)
+    let build_symbol_table stmts m =
+        List.fold_left add_symbol m stmts 
+    in
+    
+    let type_of_identifier m s =
+        try StringMap.find s m
+        with Not_found -> raise (Failure ("undeclared identifier " ^ s))
+    in
+
+
+    let check_block m stmts =
+        let m' = build_symbol_table stmts m in
+        let x = check_stmt m' stmts
+        List.map x
+    in
+
 
     let check_assign lvaluet rvaluet err =
        if lvaluet = rvaluet then lvaluet else raise (Failure err)
     in   
+
+
 
     let function_data s = 
       try StringMap.find s function_decls
       with Not_found -> raise (Failure ("unrecognized function " ^ s))
     in
 
-
-
-    
-    let id_type s =
-      try StringMap.find s symbols
-      with Not_found -> raise (Failure ("undeclared identifier " ^ s))
-    in
-
-
-
-    let rec check_expr = function
+    let rec check_expr m e = match e with
           Int_Lit i -> (Int_t, SInt_Lit i)
       |   Boolean_Lit b -> (Bool_t, SBoolean_Lit b)
       | 	Float_Lit v -> (Float_t, SFloat_Lit v)
-      | 	Mat_Lit v -> (Matrix_t, SMat_Lit (List.map check_expr v))
-      | 	Id v -> (id_type v, SId v)
+      | 	Mat_Lit v -> (Matrix_t, SMat_Lit (List.map check_expr m v))
+      | 	Id v -> (type_of_identifier m v, SId v)
       | 	Binop(e1, op, e2) ->
-          let (t1, e1') = check_expr e1 
-          and (t2, e2') = check_expr e2 in
+          let (t1, e1') = check_expr m e1 
+          and (t2, e2') = check_expr m e2 in
           let same = t1 = t2 in
           let ty = match op with
             Add | Sub | Mult | Div when same && t1 = Int_t   -> Int_t
@@ -65,12 +105,12 @@ let check_program = function
           in (ty, SBinop((t1, e1'), op, (t2, e2')))
       | 	Assign(v, e) ->
           let lt = match v with
-              Id v -> id_type v
+              Id v -> type_of_identifier m v
             | _  -> raise (Failure("Invalid assignment in " ^
                             Utils.string_of_expr e))
           in
-          let (lt', v') = check_expr v in
-          let (rt, e') = check_expr e in
+          let (lt', v') = check_expr m v in
+          let (rt, e') = check_expr m e in
           let err = "illegal assignment " 
           in (check_assign lt rt err, SAssign((lt', v'), (rt, e')))
       | 	Noexpr -> (Int_t, SNoexpr)
@@ -80,14 +120,14 @@ let check_program = function
           if List.length args != param_length then
             raise (Failure ("expecting different num args in func call") )
           else let check_call (ft, _) e = 
-            let (et, e') = check_expr e in 
+            let (et, e') = check_expr m e in 
             let err = "illegal argument found "
             in (check_assign ft et err, e')  
           in 
           let args' = List.map2 check_call fd.formals args
           in (fd.sreturnType, SCall(fname, args'))
       | Unop(op, e) as ex -> 
-          let (t, e') = check_expr e in
+          let (t, e') = check_expr m e in
           let ty = match op with
             Neg when t = Int_t || t = Float_t -> t
           | Inc when t = Int_t -> t
@@ -98,40 +138,49 @@ let check_program = function
 
       |   _ -> (Int_t, SNoexpr)
     in
-    let check_bool_expr e = 
-      let (t', e') = check_expr e
+
+    let check_bool_expr m e = 
+      let (t', e') = check_expr m e
       (*and err = "expected Boolean expression in "*)
       in let err = "expected Boolean expression in " ^ Utils.string_of_expr e
         ^ " instead " ^ string_of_primitive t'
       in if t' != Bool_t then raise (Failure err) else (t', e') 
     in
 
-    let rec check_stmt = function
-      Block b -> SBlock(List.map check_stmt b)
-      | Expr e  -> SExpr(check_expr e)
-      | Return r -> SReturn(check_expr r)
-      | If(c, b1, b2) -> SIf(check_bool_expr c, check_stmt b1, check_stmt b2)
-      | For(e1, e2, e3, s) -> SFor(check_expr e1, check_bool_expr e2, check_expr e3,
-                                   check_stmt s)
-      | While(c, s) -> SWhile(check_bool_expr c, check_stmt s)
+    let rec check_stmt m stmt = match stmt with
+      Block b -> SBlock(check_block m b)
+      | Expr e  -> SExpr(check_expr m e)
+      | Return r -> SReturn(check_expr m r)
+      | If(c, b1, b2) -> SIf(check_bool_expr m c, check_stmt m b1, check_stmt m b2)
+      | For(e1, e2, e3, s) -> SFor(check_expr m e1, check_bool_expr m e2, check_expr m e3,
+                                   check_stmt m s)
+      | While(c, s) -> SWhile(check_bool_expr m c, check_stmt m s)
       | Break -> SBreak
       | Continue -> SContinue
-      | Local(d, id, e) -> SLocal(d, id, check_expr e) (* TODO needs more type checking *)
-      | MatrixDecl(p, id, r, c, e) -> SMatrixDecl(p, id, r, c, check_expr e) (* TODO *)
+      (*| Local(d, id, e) -> let symbols = set_symbol id d symbols; SLocal(d, id)  (* TODO needs more type checking *)*)
+      | Local(d, id, e) -> SLocal(d, id)  (* TODO needs more type checking *)
+      (*| MatrixDecl(p, id, r, c, e) -> SMatrixDecl(p, id, r, c, check_expr e) (* TODO *)*)
 
   in
-    let check_fdecl f =
+
+
+    let check_fdecl m f =
         {
-          sfname = f.fname;
+        sfname = f.fname;
         sreturnType = f.returnType;
         sformals = f.formals;
-        sbody = List.map check_stmt f.body;
+        sbody = check_block m f.body;
         }
-
   in
-    let check_top_stmt = function
-        Statement stmt -> SStatement(check_stmt stmt)
-      | Function f -> SFunction(check_fdecl f)
 
+    let check_top_stmt m t_stmt = match t_stmt with
+        Statement stmt -> SStatement(check_stmt m stmt)
+      | Function f -> SFunction(check_fdecl m f)
 
-  in SProgram(includes, List.map check_top_stmt top_stmts)
+        
+
+  in 
+  
+  let globals =
+      build_symbol_table top_stmts StringMap.empty
+  in SProgram(includes, List.map check_top_stmt globals top_stmts)
