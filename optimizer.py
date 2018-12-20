036f5990ae3180afefa0d0a0442dbdf30adcfefe
dns43 <dennis.roellke@rub.de>
2018-12-20 04:45:15 +0100

    constant folding

---
+++ optimizer.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+# find llvmpy API here: https://github.com/llvmpy/llvmpy/blob/master/llvm/core.py
+
+#import IPython
+
+from llvm import *
+from llvm.core import *
+from collections import defaultdict
+import enum
+import sys
+import logging
+import pdb
+
+#mod = Module.from_assembly(file("/home/dns43/coursework/Compilers/sprint/mc_program.ll"))
+
+f_counter = 0;
+bb_counter = 0;
+insn_counter = 0;
+
+constants = {}
+
+def replace_constants(mod, bb):
+    for insn in bb.instructions:
+# if the variable is used, it gets replaced with its constant value
+        if insn.opcode == OPCODE_ADD:
+            for o in insn.operands:
+                for c in constants:
+                    if o == c:
+                        builder = Builder.new(bb)
+                        builder.position_before(insn)
+                        builder.add(insn.opcode[0], c.value(), insn.name)
+                        break
+
+def collect_constants(mod, bb):
+    global bb_counter
+    global constants 
+    
+    bb_counter += 1;
+    for insn in bb.instructions:
+        global insn_counter
+        insn_counter += 1;
+        print("instr : " + str(insn))
+
+# A variable gots defined, and assigned a constant value
+        if insn.opcode == OPCODE_ALLOCA:
+            print(insn.name)
+            print(dir(insn))
+            ninsn = insn.next()
+            if ninsn.opcode == OPCODE_STORE:
+                if isinstance(insn.operands[0], ConstantInt):
+                    constants[insn] = insn.operands[0]
+
+# The re-definitions of a variable kills its liveness
+        if insn.opcode == OPCODE_STORE:
+            for c in constants:
+                if insn.operands[1] == c:
+                    constants.pop(insn)
+
+
+
+
+
+mod = Module.from_bitcode(file("testcase/llvm-mod-firmware.bc"))
+functions = mod.functions
+for f in functions:
+    f_counter += 1;
+    bb = f.entry_basic_block
+    bb_counter += 1;
+    while True:
+        bb = collect_constants(mod, bb)
+        if not bb: break
+    if constants:
+        while True:
+            bb = replace_constants(mod, bb)
+            if not bb: break
+
+
